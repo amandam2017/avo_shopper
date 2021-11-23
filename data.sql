63b503d7731cfd024857b355b3d94deacb67f568
amandam2017 <amandamaarman19911@gmail.com>
2021-11-23 17:07:10 +0200

    initial commit

--- data.sql
+++ data.sql
@@ -3,4 +3,3 @@ insert into shop (id, name) values (2, 'Tsjekkers');
 insert into shop (id, name) values (3, 'Shopryte');
 insert into shop (id, name) values (4, 'Pick n Play');
 insert into shop (id, name) values (5, 'Fruit lovers');
-
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
 
+const AvoShopper = require('./avo-shopper');
+const pg = require('pg');
+const Pool = pg.Pool;
+
 const app = express();
 const PORT =  process.env.PORT || 3019;
 
@@ -8,21 +12,73 @@ const PORT =  process.env.PORT || 3019;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//connect to the database with connection string
+const connectionString = process.env.DATABASE_URL || 'postgresql://codex:pg123@localhost:5432/avo_shopper';
+ const pool = new Pool({
+	 connectionString
+ });
+
+const avoshopper = AvoShopper(pool);
+ 
 // enable the static folder...
 app.use(express.static('public'));
 
 // add more middleware to allow for templating support
 
-app.engine('handlebars', exphbs.engine());
+const hbs = exphbs.create();
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 let counter = 0;
 
-app.get('/', function(req, res) {
+//home route to display top 5 deals
+app.get('/', async function(req, res) {
+
+	const topDeals = await avoshopper.topFiveDeals();
+	// console.log('deals????'+await avoshopper.topFiveDeals())
 	res.render('index', {
-		counter
+		topDeals,
+		// counter
 	});
 });
+
+//add deal and a post route to submit deal to db
+app.get('/deal/add', async function(req, res) {
+	const shops = await avoshopper.listShops();
+	res.render('deal/add', {
+		shops
+	});
+});
+
+app.post('/deal/add:deals', function(req,res){
+	console.log(req.body);
+	const dealsValues = req.params.deals
+	avoshopper.createDeal(dealsValues);
+	res.redirect('/');
+});
+
+//add shop
+app.get('/shop/add', function(req, res) {
+
+	res.render('shop/add', {
+	});
+});
+
+app.post('/shop/add', function(req,res){
+	console.log(req.body);
+	avoshopper.createShop(req.body.name);
+	res.redirect('shop/add');
+});
+
+
+app.get('/shop/list', async function(req, res) {
+	const shops = await avoshopper.listShops();
+	res.render('shop/list', {
+		shops
+	});
+});
+
+
 
 // start  the server and start listening for HTTP request on the PORT number specified...
 app.listen(PORT, function() {
